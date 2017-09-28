71a981e1ffcb24ddce946a3b2696846d474df386
mdimolianis <mdimolianis@netmode.ntua.gr>
2017-09-28 18:01:33 +0300

    bgp_speaker_flowspec first try

--- ryu/app/bgp_speaker_flowspec.py
+++ ryu/app/bgp_speaker_flowspec.py
@@ -36,17 +36,17 @@ while True:
     print "add a new prefix", prefix
     speaker.flowspec_prefix_add(
         flowspec_family='ipv4fs',
-        # rules=
-        # {'dst_prefix': '147.102.13.199/32'},
-        # actions=
-        # {'traffic_rate':
-        #      { 'as_number' : 64512 ,
-        #       'rate_info': 0}}
         rules=
-        {'dst_prefix': '172.16.1.3/32'},
+        {'dst_prefix': '147.102.13.199/32'},
         actions=
-        {'traffic_marking':
-             { 'dscp': 24}}
+        {'traffic_rate':
+             { 'as_number' : 1000 ,
+              'rate_info': 0}}
+        # rules=
+        # {'dst_prefix': '172.16.1.3/32'},
+        # actions=
+        # {'traffic_marking':
+        #      { 'dscp': 24}}
 
     )
     print "-----"
