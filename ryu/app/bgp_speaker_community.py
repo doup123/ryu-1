5491591402a7fc2c0651f3e06d36a7077da660a2
mdimolianis <mdimolianis@netmode.ntua.gr>
2017-12-07 17:31:07 +0200

    lk;

--- ryu/app/bgp_speaker_community.py
+++ ryu/app/bgp_speaker_community.py
@@ -2,7 +2,6 @@ import eventlet
 
 # BGPSpeaker needs sockets patched
 eventlet.monkey_patch()
-
 # initialize a log handler
 # this is not strictly necessary but useful if you get messages like:
 #    No handlers could be found for logger "ryu.lib.hub"
@@ -10,9 +9,9 @@ import logging
 import sys
 log = logging.getLogger()
 log.addHandler(logging.StreamHandler(sys.stderr))
-
+from ryu.lib.packet.bgp import *
 from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker
-
+from ryu.services.protocols.bgp.info_base.base import AttributeMap,PrefixFilter
 def dump_remote_best_path_change(event):
     print 'the best path changed:', event.remote_as, event.prefix,\
         event.nexthop, event.is_withdraw
@@ -29,7 +28,12 @@ speaker.neighbor_add('147.102.13.156',1000)
 
 # uncomment the below line if the speaker needs to talk with a bmp server.
 # speaker.bmp_server_add('192.168.177.2', 11019)
-speaker.attribute_map_set("147.102.13.156")
+pref_filter = PrefixFilter('147.102.0.0/16',PrefixFilter.POLICY_PERMIT)
+
+attribute_map = AttributeMap([pref_filter],
+                                     AttributeMap.ATTR_LOCAL_PREF, 250)
+
+speaker.attribute_map_set("147.102.13.156",attribute_maps=BGP_COMMUNITY_NO_EXPORT)
 count = 1
 while True:
     eventlet.sleep(30)
