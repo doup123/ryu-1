e883d5b8dcdf5760f4a3318a9a4266def76de56f
marinos <mdimolianis@netmode.ntua.gr>
2018-01-20 19:53:31 +0200

    blah

--- ryu/app/bgp_speaker_flowspec.py
+++ ryu/app/bgp_speaker_flowspec.py
@@ -2,7 +2,6 @@ import eventlet
 
 # BGPSpeaker needs sockets patched
 eventlet.monkey_patch()
-
 # initialize a log handler
 # this is not strictly necessary but useful if you get messages like:
 #    No handlers could be found for logger "ryu.lib.hub"
@@ -10,9 +9,9 @@ import logging
 import sys
 log = logging.getLogger()
 log.addHandler(logging.StreamHandler(sys.stderr))
-
+from ryu.lib.packet.bgp import *
 from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker
-
+from ryu.services.protocols.bgp.info_base.base import AttributeMap,PrefixFilter
 def dump_remote_best_path_change(event):
     print 'the best path changed:', event.remote_as, event.prefix,\
         event.nexthop, event.is_withdraw
@@ -29,26 +28,36 @@ speaker.neighbor_add('147.102.13.156',1000,enable_ipv4fs=True)
 
 # uncomment the below line if the speaker needs to talk with a bmp server.
 # speaker.bmp_server_add('192.168.177.2', 11019)
+# pref_filter = PrefixFilter('147.102.0.0/16',PrefixFilter.POLICY_PERMIT)
+
+# attribute_map = AttributeMap([pref_filter],
+#                                      AttributeMap.ATTR_LOCAL_PREF, 250)
+# speaker.prefix_del()
+#speaker.attribute_map_set("147.102.13.156",attribute_maps=attribute_map)
+from time import time
+list=[]
 count = 1
 while True:
-    eventlet.sleep(30)
-    prefix = '10.20.' + str(count) + '.0/24'
-    print "add a new prefix", prefix
-    speaker.flowspec_prefix_add(
-        flowspec_family='ipv4fs',
-        rules=
-        {'src_prefix': '147.102.13.199/32'},
-        actions=
-        {'traffic_rate':
-             {'rate_info': 0}}
-        # rules=
-        # {'dst_prefix': '172.16.1.3/32'},
+    x=time()
+    for i in range(1, 99):
+        speaker.flowspec_prefix_add(
+            flowspec_family='ipv4fs',
+            rules=
+            {'src_prefix': '147.102.13.' + str(i) + '/32'},
+            actions=
+            {'traffic_rate':
+                {'rate_info': 0}})
+    # {'dst_prefix': '172.16.1.3/32'},
         # actions=
         # {'traffic_marking':
         #      { 'dscp': 24}}
-
-    )
+    y=time()
+    list.append(y-x)
+    print "-----"
+    print "-----"
     count += 1
     if count == 4:
+
         speaker.shutdown()
+        print list
         break
